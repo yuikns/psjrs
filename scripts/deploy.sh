ea4f84883570496e037101c6f83ca55087f3fbb0
Yu Jing <yujing5b5d@gmail.com>
2015-09-30 21:20:38 +0800

    update deploy scripts

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sbt compile dist
-ln -sf `pwd`/scripts/as_launcher.sh target/universal/
-cd target/universal/ && unzip argcv-scholar-1.0.0.zip && ln -sf argcv-scholar-1.0.0 argcv-scholar-current
-
